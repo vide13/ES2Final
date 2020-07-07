9ab1b8bb0b42b3288c866ea5bc683b515be8067f
Filipe Ribeiro <f.ribeiro.redocean@gmail.com>
2020-07-07 20:03:12 +0100

    SingleUserTest done

---
+++ src/test/java/com/es3/client/SingleUserTest.java
@@ -0,0 +1,31 @@
+package com.es3.client;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class SingleUserTest { 
+
+    @Test
+    void singleUserIdNull() {
+        assertThrows(Error.class, () -> Client.singleUser(null));
+    }
+
+    @Test
+    void singleUserBigId() {
+        assertThrows(Error.class, () -> Client.singleUser(1000000));
+    }
+
+    @Test
+    void singleUserIdLesserThanZero() {
+        assertThrows(Error.class, () -> Client.singleUser(-5));
+    }
+
+    @Test
+    void singleUserSuccessful() {
+        assertDoesNotThrow(() -> Client.singleUser(2));
+    }
+
+
+}
