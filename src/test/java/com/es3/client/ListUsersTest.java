dbab2a790634bfd3bbc293b97cd00a4d1eb36e9d
Filipe Ribeiro <f.ribeiro.redocean@gmail.com>
2020-07-07 20:15:16 +0100

    ListUsersTest done

---
+++ src/test/java/com/es3/client/ListUsersTest.java
@@ -0,0 +1,16 @@
+package com.es3.client;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ListUsersTest {
+
+    @Test
+    void singleUserSuccessful() {
+        assertDoesNotThrow(() -> Client.listUsers());
+    }
+
+
+}
--- src/test/java/com/es3/client/SingleUserTest.java
+++ src/test/java/com/es3/client/SingleUserTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-class SingleUserTest { 
+class SingleUserTest {
 
     @Test
     void singleUserIdNull() {
