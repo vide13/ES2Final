f8ae3a407c4543adcdf4f9729001a020f8a5c899
Jorge Cravid <estgv15232@alunos.estgv.ipv.pt>
2020-07-08 10:14:25 +0100

    Service: Implemented ListResourcesTest and ListUsersTest.

--- src/test/java/com/es3/service/ListResourcesTest.java
+++ src/test/java/com/es3/service/ListResourcesTest.java
@@ -1,4 +1,23 @@
 package com.es3.service;
 
+import com.es3.HTTPClient.Retrofit;
+import com.es3.controller.Endpoint;
+import com.es3.objects.ListResources;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import retrofit2.Call;
+import retrofit2.Response;
+
+import java.io.IOException;
+
 public class ListResourcesTest {
+    @Test
+    void listResources() throws IOException {
+        Endpoint endpoint = Retrofit.getClient().create(Endpoint.class);
+        Call<ListResources> request = endpoint.listResources();
+        Response<ListResources> response = request.execute();
+        Assertions.assertEquals(200, response.code());
+        Assertions.assertNotNull(response.body());
+    }
+
 }
--- src/test/java/com/es3/service/ListUsersTest.java
+++ src/test/java/com/es3/service/ListUsersTest.java
@@ -1,5 +1,22 @@
 package com.es3.service;
 
-public class ListUsersTest {
+import com.es3.HTTPClient.Retrofit;
+import com.es3.controller.Endpoint;
+import com.es3.objects.ListUsers;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import retrofit2.Call;
+import retrofit2.Response;
 
+import java.io.IOException;
+
+public class ListUsersTest {
+    @Test
+    void listUsers() throws IOException {
+        Endpoint endpoint = Retrofit.getClient().create(Endpoint.class);
+        Call<ListUsers> request = endpoint.listUsers();
+        Response<ListUsers> response = request.execute();
+        Assertions.assertEquals(200, response.code());
+        Assertions.assertNotNull(response.body());
+    }
 }
