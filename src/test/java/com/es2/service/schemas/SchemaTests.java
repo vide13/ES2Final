41f367e1414be124f1cf733173e74e33828f47e6
João Aragão <emptechno@gmail.com>
2020-07-23 14:56:16 +0100

    schemas done for service testing

---
+++ src/test/java/com/es2/service/schemas/SchemaTests.java
@@ -0,0 +1,54 @@
+package com.es2.service.schemas;
+
+import org.jsmart.zerocode.core.domain.Scenario;
+import org.jsmart.zerocode.core.domain.TargetEnv;
+import org.jsmart.zerocode.core.runner.ZeroCodeUnitRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@TargetEnv("SchemaTests/SchemaTest.properties")
+@RunWith(ZeroCodeUnitRunner.class)
+public class SchemaTests {
+
+    @Scenario("SchemaTests/SchemaCreateUserTest.json")
+    @Test
+    public void SchemaCreateUserTest() {
+
+    }
+
+    @Scenario("SchemaTests/SchemaListResourcesTest.json")
+    @Test
+    public void SchemaListResourcesTest() {
+
+    }
+
+    @Scenario("SchemaTests/SchemaListUsersTest.json")
+    @Test
+    public void SchemaListUsersTest() {
+
+    }
+
+    @Scenario("SchemaTests/SchemaLoginUserTest.json")
+    @Test
+    public void SchemaLoginUserTest() {
+
+    }
+
+    @Scenario("SchemaTests/SchemaRegisterUserTest.json")
+    @Test
+    public void SchemaRegisterUserTest() {
+
+    }
+
+    @Scenario("SchemaTests/SchemaSingleResourceTest.json")
+    @Test
+    public void SchemaSingleResourceTest() {
+
+    }
+
+    @Scenario("SchemaTests/SchemaSingleUserTest.json")
+    @Test
+    public void SchemaSingleUserTest() {
+
+    }
+}
